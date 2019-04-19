777cf09b4f8011a21775c1d6003bb7b64e85f5a6
athiiii <theivakulasingham@outlook.de>
2019-04-19 16:47:55 +0200

    define start page

--- MemeGenMgmt/MGM.API/Startup.cs
+++ MemeGenMgmt/MGM.API/Startup.cs
@@ -1,4 +1,5 @@
-﻿using MGM.API.Middleware;
+﻿using System.Collections.Generic;
+using MGM.API.Middleware;
 using MGM.CQRS;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -41,6 +42,14 @@ namespace MGM.API
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseDefaultFiles(new DefaultFilesOptions
+            {
+                DefaultFileNames = new List<string>
+                {
+                    "index.html"
+                }
+            });
             app.AddExceptionHandler(_logger);
 
             app.UseHttpsRedirection();
